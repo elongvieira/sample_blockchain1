1eb7b2a12a763f6b1c8a6888a7b76e4288a4056c
Elon Gomes Vieira <elongvieira@yahoo.com.br>
2022-03-06 11:30:06 -0300

    add method isValidChain

--- blockchain.test.js
+++ blockchain.test.js
@@ -4,9 +4,11 @@ const Block = require('./block');
 
 describe('Blockchain', () => {
     let blockchain;
+    let blockchain2;
 
     beforeEach(() => {
         blockchain = new Blockchain();
+        blockchain2 = new Blockchain();
     });
 
     it('starts with the genesis block', () => {
@@ -18,5 +20,24 @@ describe('Blockchain', () => {
         blockchain.addBlock(data);
 
         expect(blockchain.chain[blockchain.chain.length - 1].data).toEqual(data);
+    });
+
+    it('validates a valid chain', () => {
+        blockchain2.addBlock('500U$');
+
+        expect(blockchain.isValidChain(blockchain2.chain)).toBe(true);
+    });
+
+    it('invalidate a chain with a currupt genesis block', () => {
+        blockchain2.chain[0].data = '0U$';//corrupt
+
+        expect(blockchain.isValidChain(blockchain2.chain)).toBe(false);
+    });
+
+    it('invalidate a corrupt chain', () => {
+        blockchain2.addBlock('500U$');
+        blockchain2.chain[1].lastHash = '0U$';//corrupt
+
+        expect(blockchain.isValidChain(blockchain2.chain)).toBe(false);
     });
 })
