0780ffa882e9ef1f50607acbc1bbc1f6745619c7
Elon Gomes Vieira <elongvieira@yahoo.com.br>
2022-04-03 10:43:44 -0300

    add route mine

--- app/index.js
+++ app/index.js
@@ -5,8 +5,16 @@ const HTTP_PORT = process.env.HTTP_PORT || 3001; // $HTTP_PORT = 3002 npm run de
 const app = express();
 const blockchain = new Blockchain();
 
-app.get('blocks', (req, res) => {
+app.use(express.json());
+
+app.get('/blocks', (req, res) => {
     res.json(blockchain.chain);    
+});
+
+app.post('/mine', (req, res) => {
+    const block = blockchain.addBlock(req.body.data);
+    console.log(`New block added: ${block.toString()}`);
+    res.redirect('/blocks');
 });
 
 app.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}`));
